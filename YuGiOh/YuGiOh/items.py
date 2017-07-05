103aa01050bdebb4eb64f7caa89886ee60f9056a
yubin <1575529757@qq.com>
2017-07-05 11:25:21 +0800

    要爬取的属性

--- YuGiOh/YuGiOh/items.py
+++ YuGiOh/YuGiOh/items.py
@@ -11,4 +11,11 @@ import scrapy
 class YugiohItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    name = scrapy.Field()  # 名字
+    lv = scrapy.Field()  # 等级
+    atk = scrapy.Field()  # 攻击力
+    Def = scrapy.Field()  # 防御力
+    effect = scrapy.Field()  # 效果
+    attribute = scrapy.Field()  # 属性
+    tribe = scrapy.Field()  # 种族
     pass
