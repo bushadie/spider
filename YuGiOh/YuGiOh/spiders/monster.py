7c3006b8ddca2a00ef166ac714a789195fb722ee
yubin <1575529757@qq.com>
2017-07-05 19:28:22 +0800

    残缺的较完整怪兽版，链接，灵摆不行

--- YuGiOh/YuGiOh/spiders/monster.py
+++ YuGiOh/YuGiOh/spiders/monster.py
@@ -2,6 +2,11 @@ from scrapy.spiders import Spider
 from ..items import YugiohItem
 from scrapy import Request
 
+import sys
+
+reload(sys)
+
+sys.setdefaultencoding('utf-8')
 
 class YuGiOhMonsterSpider(Spider):
     name = 'YuGiOhMonsterSpider'
@@ -20,30 +25,43 @@ class YuGiOhMonsterSpider(Spider):
         cards = response.xpath('//div[@card_id]')
         i = 2
         for card in cards:
-            item['name'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/h2/a/text()'
-            ).extract()[0]
-            item['lv'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/div[1]/span[2]/text()'
-            ).extract()[0]
-            item['atk'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[2]/text()'
-            ).extract()[0]
-            item['Def'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[4]/text()'
-            ).extract()[0]
-            item['tribe'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[6]/text()'
-            ).extract()[0]
-            item['attribute'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[8]/text()'
-            ).extract()[0]
-            item['effect'] = card.xpath(
-                '//section/div[' + str(i) + ']/div/section/div/p/text()'
-            ).extract()[0]
+            try:
+                item['name'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/h2/a/text()'
+                ).extract()[0]
+                item['lv'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/div[1]/span[2]/text()'
+                ).extract()[0]
+                item['atk'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[2]/text()'
+                ).extract()[0]
+                item['Def'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[4]/text()'
+                ).extract()[0]
+                item['tribe'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[6]/text()'
+                ).extract()[0]
+                item['attribute'] = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/div[1]/span[4]/span[8]/text()'
+                ).extract()[0]
 
-            print i
-            i += 1
-            yield item
+                effects = card.xpath(
+                    '//section/div[' + str(i) + ']/div/section/div/p/text()'
+                ).extract()
+                item['effect'] = ''
+                for effect in effects:
+                    item['effect'] += effect
+                item['effect'] = item['effect'].replace('\r\n', '')
+
+                print i
+                i += 1
+                yield item
+            except:
+                print '-----------------error-------------------------'
+        next_url = response.xpath('//*[@id="main"]/div/div/div/section/aside[3]/p/span[3]/a[1]/@href').extract()
+        if next_url:
+            i = 2
+            next_url = 'http://www.ourocg.cn' + next_url[0]
+            yield Request(next_url, headers=self.headers)
 
     pass
