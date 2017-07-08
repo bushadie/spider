e1eab53525e4267e558aa24919df05b8ab65eafe
yubin <1575529757@qq.com>
2017-07-08 16:43:19 +0800

    Python 模拟浏览器

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,12 @@
+# -*- coding:utf-8 -*-
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome()
 browser.get('http://www.baidu.com/')
+
+time.sleep(2)
+browser.get('http://www.bilibili.com/')  # open page
+browser.refresh()  # F5
+# browser.find_element_by_id('i_menu_login_btn').send_keys('123456');
+browser.find_element_by_link_text(u"登录").click()  # login
